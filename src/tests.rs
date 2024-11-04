9aeabc13ad8a4759d1bf39d034e249f606d8bf39
Skye Kaijser <skye@kth.se>
2024-11-04 14:38:38 +0100

    machine code parser done

--- src/tests.rs
+++ src/tests.rs
@@ -1,17 +1,25 @@
 #[cfg(test)]
 mod tests {
+    use crate::opcodes::*;
+
     // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::*;
-
+    
     #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
+    fn test_line() {
+        let oc:OpCode=parse_oc("0FFF".to_owned());
+        assert_eq!((oc.op_code,oc.oc_type,oc.oc_id),(0x0FFF,OpCodeType::CALL(1),OpCodeIdentity::CallMach));
+    }
+    #[test]
+    fn test_text() {
+        let ocs:Vec<OpCode>=parse_text("0FFF\n0222\nF355\n8AB3".to_owned());
+        let cmpvec:Vec<OpCode>=vec![
+            OpCode{oc_type:OpCodeType::CALL(1), oc_id:OpCodeIdentity::CallMach, op_code:0x0FFF},
+            OpCode{oc_type:OpCodeType::CALL(1), oc_id:OpCodeIdentity::CallMach, op_code:0x0222},
+            OpCode{oc_type:OpCodeType::MEM(16), oc_id:OpCodeIdentity::DumpRegsToMemR, op_code:0xF355},
+            OpCode{oc_type:OpCodeType::BITOP(4), oc_id:OpCodeIdentity::XorRR, op_code:0x8AB3}
+        ];
+        assert_eq!(ocs,cmpvec);
     }
 
-    #[test]
-    fn test_bad_add() {
-        // This assert would fire and test will fail.
-        // Please note, that private functions can be tested too!
-        assert_eq!(bad_add(1, 2), 3);
-    }
+
 }
